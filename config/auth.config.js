9dfc5baf618a60e88855b1cef6ef47211086c450
Jocarfl <jcarreresjr@hotmail.com>
2022-06-16 19:39:35 +0200

    Auth with JWT

---
+++ config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "josep-secret-key"
+  };
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+
+
 var corsOptions = {
   origin: "http://localhost:8081"
 };
@@ -10,6 +12,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = require("./models");
+const Role = db.role;
+
 db.mongoose
   .connect(db.url, {
     useNewUrlParser: true,
@@ -17,6 +21,7 @@ db.mongoose
   })
   .then(() => {
     console.log("Connected to the database!");
+    initial();
   })
   .catch(err => {
     console.log("Cannot connect to the database!", err);
@@ -26,8 +31,43 @@ db.mongoose
 app.get("/", (req, res) => {
   res.json({ message: "Bienvenido" });
 });
+
+require('./routes/auth.routes')(app);
+require('./routes/user.routes')(app);
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
+function initial() {
+    Role.estimatedDocumentCount((err, count) => {
+      if (!err && count === 0) {
+        new Role({
+          name: "user"
+        }).save(err => {
+          if (err) {
+            console.log("error", err);
+          }
+          console.log("added 'user' to roles collection");
+        });
+        new Role({
+          name: "medico"
+        }).save(err => {
+          if (err) {
+            console.log("error", err);
+          }
+          console.log("added 'medico' to roles collection");
+        });
+        new Role({
+          name: "admin"
+        }).save(err => {
+          if (err) {
+            console.log("error", err);
+          }
+          console.log("added 'admin' to roles collection");
+        });
+      }
+    });
+  }
